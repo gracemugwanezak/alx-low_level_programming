6d78c31f6290b50e359cbac52045f4412ffee033
Grace <gracemugwaneza018@gmail.com>
2023-10-05 16:58:25 -0700

    intel

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry stage
+ *a program that print a standard error
+ * return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
