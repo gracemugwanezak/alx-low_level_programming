3788ebb498093a4307eeced59077a09208a15348
Grace <gracemugwaneza018@gmail.com>
2023-10-10 13:58:40 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,25 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_to_98 - Print numbers from n to 98
- * @n: staert number
- * Return: 0
- */
+*print_to_98 - prints natural
+*numbers from n to 98
+*@n:integer to start by
+*return: returns nothing
+*/
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		while (n < 98)
-		{
-			printf("%d,", n);
-			n++;
-		}
-	}
-	else if (n > 98)
-	{
-		while (n > 98)
-		{
-			printf("%d,", n);
-			n--;
-		}
-	}
-	printf("98\n");
+if (n >= 98)
+{
+while (n > 98)
+printf("%d, ", n--);
+
+printf("%d\n", n);
+}
+else
+{
+while (n < 98)
+printf("%d, ", n++);
+
+printf("%d\n", n);
+}
 }
