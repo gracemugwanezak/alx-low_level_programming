99d541dc60fc95af753452f1667f8211597955b1
Grace <gracemugwaneza018@gmail.com>
2023-10-10 07:53:30 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+	_putchar('\n');
+}
