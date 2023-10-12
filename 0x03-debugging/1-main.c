a8f8dfa73eb58c6c19e35c7369ca5dcc795a0c9b
Grace <gracemugwaneza018@gmail.com>
2023-10-12 02:50:37 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,4 +1,3 @@
-carrie@ubuntu:/debugging$ cat 1-main.c
 #include <stdio.h>
 
 /**
@@ -8,20 +7,17 @@ carrie@ubuntu:/debugging$ cat 1-main.c
 
 int main(void)
 {
-        int i;
+	int i;
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
-/*
-
-       * while (i < 10)
-       *  {
-       *        putchar(i);
-       *  }
-*/
-        printf("Infinite loop avoided! \\o/\n");
-
-        return (0);
+	i = 0;
+	/*
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
 }
-carrie@ubuntu:/debugging$
