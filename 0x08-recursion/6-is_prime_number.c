a12ef39c23ee7ff7edf874eb5c058db6403d23dd
Grace <gracemugwaneza018@gmail.com>
2023-10-25 13:55:29 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int evaluate_prime(int n, int i);
+
+/**
+ * is_prime_number - evaluate if number is prime
+ * @n: number to evaluate
+ * Return: return 1 or 0
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (1);
+	return (evaluate_prime(n, n - 1));
+}
+/**
+ * evaluate_prime - recursion loop
+ * @n: number to evaluate
+ * @i: iterator
+ * Return: return 1 if number is prime, 0 if not
+ */
+int evaluate_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (evaluate_prime(n, i - 1));
+}
